9a5dcdbe24f96c1bcfdc8f1874a470e728126f02
Lawrence Rivales <lrivales@gmail.com>
2021-09-17 22:42:34 -0400

    added >30 issues link

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(owner,repo) {
     var apiUrl = "https://api.github.com/repos/"+owner+"/"+repo+"/issues?direction=asc";
@@ -9,6 +10,11 @@ var getRepoIssues = function(owner,repo) {
             response.json().then(function(data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(owner, repo)
+                }
             });
         } else {
             alert("There was a problem with your request!")
@@ -46,6 +52,19 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+var displayWarning = function(owner, repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Github.com";
+    linkEl.setAttribute("href","https://api.github.com/repos/"+owner+"/"+repo+"/issues");
+    linkEl.setAttribute("target","_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 }
 
 getRepoIssues("octocat","git-consortium");
